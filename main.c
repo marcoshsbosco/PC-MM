c424111f33b6485be7eac8f6823dd8285e7b1d21
Bosco <58055884+marcoshsbosco@users.noreply.github.com>
2023-07-11 20:26:50 -0300

    Versão inicial para prototipar o projeto

---
+++ main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+
+int nfluxos;
+int n;
+
+int *mA;
+int *mB;
+int *mC;
+
+
+void *worker(void *arg) {
+    int tnum = *(int *)arg;
+
+    for (int i = tnum * n / nfluxos; i < (n + n * tnum) / nfluxos; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                mC[n * i + j] += mA[n * i + k] * mB[n * k + j];
+            }
+        }
+    }
+
+    return NULL;
+}
+
+
+int main(int argc, char *argv[]) {
+    pthread_t *tid;
+    int *tnum;
+
+    if (argc != 3) {
+        printf("--Uso CLI--\n./a.out nfluxos nmatriz\n");
+
+        return 1;
+    } else {
+        nfluxos = strtol(argv[1], NULL, 10);
+        n = strtol(argv[2], NULL, 10);
+    }
+
+    // alocação de memória
+    tid = calloc(nfluxos, sizeof(pthread_t));
+    tnum = calloc(nfluxos, sizeof(int));
+    mA = calloc(n * n, sizeof(int));
+    mB = calloc(n * n, sizeof(int));
+    mC = calloc(n * n, sizeof(int));
+
+    for (int i = 0; i < n * n; i++) {
+        mA[i] = i;
+        mB[i] = i;
+        mC[i] = 0;
+    }
+
+    // criação de fluxos e de seus atributos (ID e número)
+    for (int i = 0; i < nfluxos; i++) {
+        tnum[i] = i;
+        pthread_create(&tid[i], NULL, &worker, &tnum[i]);
+    }
+
+    // join
+    for (int i = 0; i < nfluxos; i++) {
+        pthread_join(tid[i], NULL);
+    }
+
+    // print matriz C
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", mC[n * i + j]);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,3 @@
+make:
+	@clear
+	@gcc -pthread main.c
